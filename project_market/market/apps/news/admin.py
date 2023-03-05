e1e87ce071baffe81b8b2cc71d2c926e1e8f7ded
ribla9000 <2001rna.mail@mail.ru>
2023-03-05 04:06:04 +0300

    updated 05.03-04:05

--- project_market/market/apps/news/admin.py
+++ project_market/market/apps/news/admin.py
@@ -5,4 +5,5 @@ from .models import News
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
 	change_list_template = "admin/news_change_list.html"
-	list_display = ('tag', 'title', 'detail')
+	list_display = ('id', 'tag', 'title', 'detail')
+	list_display_links = ('id', 'tag', 'title')
--- project_market/market/apps/news/managment/commands/parsing/text_changer.py
+++ project_market/market/apps/news/managment/commands/parsing/text_changer.py
@@ -2,4 +2,6 @@ async def text_validator(param):
 	for i in range(len(param)-1):
 		if param[i].islower() and param[i+1].isupper():
 			param = f"{param[:i+1]}. {param[i+1:]}"
+		if isinstance(param[i], str) and isinstance(param[i+1], int):
+			param = f"{param[:i + 1]}. {param[i + 1:]}"
 	return param
